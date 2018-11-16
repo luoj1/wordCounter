8597020a300dc5616d6ae4766981e586d7d5ef35
luoj1 <2404551877@qq.com>
2018-11-16 14:13:39 -0500

    order number

--- wordCounter.py
+++ wordCounter.py
@@ -14,18 +14,23 @@ def wordCounter(src, target):
 			line = line[:-1];
 			# regex that replace everything that is not letter, number, dash or hyphen by space (' ')
 			# split the processed line by space
-			line =  re.sub("[^0-9&^a-z&^A-Z&^\-&^\_]", " ", line).split() 
+			line =  re.sub("[^0-9^a-z^A-Z^\-^\_]", " ", line).split() 
 			for word in line:
+				word = word.lower()
+				#check if word is a pure number
+				if re.sub("[0-9\-\_]", "", word) == "":
+					continue
 				if word in wordDictionary:
-					wordDictionary[word] = wordDictionary[word] + 1
+					wordDictionary[word][0] += 1
 				else:
-					wordDictionary[word] = 1
+					wordDictionary[word] = [1, word]
 			line = file.readline()
-
+	wordPair = [wordDictionary[key] for key in wordDictionary]
+	wordPair = sorted(wordPair, key=lambda x: (x[0],ord('a') - ord(x[1][0])), reverse=True)
 	with open(target, 'w') as writer:
-		for word in wordDictionary:
-			writer.write(word + ' ')
-			writer.write(str(wordDictionary[word]))
+		for pair in wordPair:
+			writer.write(str(pair[0]) + ' ')
+			writer.write(pair[1])
 			writer.write('\n')
 
 
